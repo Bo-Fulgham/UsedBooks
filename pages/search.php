11efb3fb7be7210fd0a952f1732b2b18fd073698
dharrison582 <33530391+dharrison582@users.noreply.github.com>
2017-11-27 15:57:37 -0600

    Changed Search

--- pages/search.php
+++ pages/search.php
@@ -5,23 +5,126 @@
 		<title>Search</title>
 	</head>
 	<body>
-		<?php require '../functions/header.php';?>
 		<h2>this is the search page</h2>
-		<?php require '../functions/pagesbar.php';?>
+		<?php //require '../functions/pagesbar.php';
+            echo "
+    <p> <a href='home.php'>NotChegg</a> </p>
+    <p> <a href='about.php'>About</a> </p>
+    <p> <a href='contact.php'>Contact Us</a> </p> ";
+    if (isset($_SESSION['logged_in'])){
+        if ($_SESSION['logged_in'] == true){
+            echo "<p><a href='logout.php'>Logout</a> </p>";
+        }
+        else {
+            echo "<p><a href='login_page.php'>Login</a></p>
+                <p><a href='registration_page.php'>Register</a></p>";
+        }
+    }
+    else {
+        echo "<p><a href='login_page.php'>Login</a></p>
+        <p><a href='registration_page.php'>Register</a></p>";
+    }
+echo "
+    <p> <a href='account.php'>Account</a></p>
+    <form method='get' action='search.php'>
+    <p>Search: <input name='search_terms' type='text' size='25' maxlength='30'> </p>
+    <p>
+        <!-- input types 'submit' and 'reset' insert  -->
+        <!-- buttons for submitting and clearing the  -->
+        <!-- form's contents                          --> 
+        <input type='submit' value='Submit'>
+    </p> </form>
+    <p> <a href='Cart.php'>Cart</a> </p>
+    ";
+        
+        
+              require_once '../functions/login.php';
 
-		<?php
+                $conn = new mysqli($hn, $un, $pw, $db);
+                if ($conn->connect_error) 
+                    die($conn->connect_error);
+                $terms = $_GET['search_terms'];
 
-            session_start();
-            // is an Admin logged in?
-            if($_SESSION['logged_in'] == true){
-                if ($_SESSION['type'] == 'user') {
-                    header('Location: user_page.php');
-                }
-                echo "admin stuff";
-            }
-            else {
-                echo "You are not logged in. <br>" . '<a href="login_page.php">Log in here</a>';
-            }         
+                //Search 
+                $query  = "SELECT * FROM inventory WHERE title LIKE '%$terms%'";
+                $result = $conn->query($query);
+                if (!$result) 
+                     die($conn->error);
+
+                 $rows = $result->num_rows;
+
+                    for ($j = 0 ; $j < $rows ; ++$j)
+                    {
+                        $result->data_seek($j);
+                        $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                    echo 'BookID: '   . $row['bookID']   . '<br>';
+                    echo 'ISBN: '    . $row['ISBN']    . '<br>';
+                    echo 'Title: ' . $row['Title'] . '<br>';
+                    echo 'Author: '     . $row['Author']     . '<br>';
+                    echo 'Publisher: '     . $row['Publisher']. '<br>';
+                    echo 'Price:' .$row['Price'] . ' <br>';
+                    echo 'Image?:' .$row['Image']. '<br>';
+                    echo 'isFlagged:' .$row['isFlagged']. '<br>';
+                    echo 'category:' .$row['Category']. '<br>';
+                    echo 'Quantity:'  .$row['quantity']. '<br><br><br>';
+                 
+                    }
+
+
+                    //Search AUthor
+             $query  = "SELECT * FROM inventory WHERE Author LIKE '%$terms%'";
+                $result = $conn->query($query);
+                if (!$result) 
+                     die($conn->error);
+
+                 $rows = $result->num_rows;
+
+                    for ($j = 0 ; $j < $rows ; ++$j)
+                    {
+                        $result->data_seek($j);
+                        $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                    echo 'BookID: '   . $row['bookID']   . '<br>';
+                    echo 'ISBN: '    . $row['ISBN']    . '<br>';
+                    echo 'Title: ' . $row['Title'] . '<br>';
+                    echo 'Author: '     . $row['Author']     . '<br>';
+                    echo 'Publisher: '     . $row['Publisher']. '<br>';
+                    echo 'Price:' .$row['Price'] . ' <br>';
+                    echo 'Image?:' .$row['Image']. '<br>';
+                    echo 'isFlagged:' .$row['isFlagged']. '<br>';
+                    echo 'category:' .$row['Category']. '<br>';
+                    echo 'Quantity:'  .$row['quantity']. '<br><br><br>';
+                 
+                    }
+                    //Search ISBN
+            $query  = "SELECT * FROM inventory WHERE ISBN LIKE '%$terms%'";
+                $result = $conn->query($query);
+                if (!$result) 
+                     die($conn->error);
+
+                 $rows = $result->num_rows;
+
+                    for ($j = 0 ; $j < $rows ; ++$j)
+                    {
+                        $result->data_seek($j);
+                        $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                    echo 'BookID: '   . $row['bookID']   . '<br>';
+                    echo 'ISBN: '    . $row['ISBN']    . '<br>';
+                    echo 'Title: ' . $row['Title'] . '<br>';
+                    echo 'Author: '     . $row['Author']     . '<br>';
+                    echo 'Publisher: '     . $row['Publisher']. '<br>';
+                    echo 'Price:' .$row['Price'] . ' <br>';
+                    echo 'Image?:' .$row['Image']. '<br>';
+                    echo 'isFlagged:' .$row['isFlagged']. '<br>';
+                    echo 'category:' .$row['Category']. '<br>';
+                    echo 'Quantity:'  .$row['quantity']. '<br><br><br>';
+                 
+                    }
+  $result->close();
+  $conn->close();
+         
 
         ?>
 
